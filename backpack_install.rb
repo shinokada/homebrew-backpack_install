84e52d1598b75a79c205e20781d21de963f91f45
Shinichi Okada <okada.shin@gmail.com>
2021-06-06 21:05:15 +0900

    v0.6.2

--- backpack_install.rb
+++ backpack_install.rb
@@ -1,8 +1,8 @@
 class BackpackInstall < Formula
   desc "Backpack Installer for macOS/Linux"
   homepage "https://github.com/shinokada/backpack_install"
-  url "https://github.com/shinokada/backpack_install/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "1fcb6ee9be298eb045c97c341dbda8ec64e97f96ef8f2f4f3b93b6bfd0995a07"
+  url "https://github.com/shinokada/backpack_install/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "d37878c158d9657763925c0ed7e0f4e25d2147c0581c1cec3f181884e6f5a391"
   license "MIT"
 
   bottle :unneeded
